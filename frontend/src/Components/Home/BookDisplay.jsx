fbe3e6c5d117daf9df3f4cd5a8a32b785b5dbb74
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-12-02 20:32:08 -0700

    created basic home page logic

---
+++ frontend/src/Components/Home/BookDisplay.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+
+export const BookDisplay = ({data}) => {
+    const [book, setBook] = useState(data)
+
+    useEffect(() => {
+        setBook(data)
+    }, [data])
+
+    return (
+        <>
+            <img src={book.coverImage} alt={book.title}></img>
+            <h2>{book.title}</h2>
+            <p>{book.author}</p>
+            <p>{book.description}</p>
+            <Link to="/Books/:id">See more about this book</Link>
+        </>
+    )
+}
--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,10 +1,37 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import { BookDisplay } from "./BookDisplay"
+import { useNavigate } from "react-router-dom"
 
 export const Home = () => {
+    const [books, setBooks] = useState([])
+    const navigate = useNavigate()
+
+    const GetBooks = async () => {
+        fetch('/books', {
+            method: 'GET',
+            headers: {
+                "Content-Type": "Application/JSON"
+            }
+        })
+        .then(res => res.json())
+        .then(data => setBooks(data.slice(0, 5)))
+        .catch(error => navigate(`/Error/${error}`))
+    }
+
+    useEffect(() => {
+        GetBooks()
+    }, [])
 
     return (
         <div>
-            this is the home page
+            <h1>Welcome!</h1>
+            <section>
+                {books.map(book => {
+                    return(
+                        <BookDisplay key={book.id} data={book} />
+                    )
+                })}
+            </section>
         </div>
     )
 }
