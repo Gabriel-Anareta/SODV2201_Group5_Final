16b432656d1f1522c7a21897cab7e4b650f2dc89
dbosing <d.bosing154@mybvc.ca>
2024-12-03 23:36:48 -0700

    Update AllBooks.jsx

--- frontend/src/Components/Books/AllBooks.jsx
+++ frontend/src/Components/Books/AllBooks.jsx
@@ -1,26 +1,40 @@
 import { useEffect, useState } from "react"
 import { useParams, Link, useNavigate } from "react-router-dom"
 import { useAllBooks } from "../../CustomHooks/BookHooks"
+import "./AllBooks.css";
 
 export const AllBooks = () => {
     const allBooks = useAllBooks()
 
     return (
         <div>
-            <h1>All Books</h1>
-            <section>
-                {allBooks.map(book => {
-                    return(
-                        <>
-                            <img src={book.coverImage} alt={book.title}></img>
-                            <h2>{book.title}</h2>
-                            <p>{book.author}</p>
-                            <p>{book.description}</p>
-                            <Link to={`/Books/${book.id}`}>See more about this book</Link>
-                        </>
-                    )
-                })}
-            </section>
+            <header>
+                <h1>All Books</h1>
+                <div className="search-filter">
+                    <input type="text" placeholder="Search books..." />
+                    <button>Filter ▼</button>
+                </div>
+            </header>
+            <main>
+                <div className="book-grid">
+                    {allBooks.map(book => {
+                        return(
+                            <div className="book-container" key={book.id}>
+                                <div className="book-card" key={book.id}>
+                                    <img src={book.coverImage} alt={book.title}></img>
+                                </div>
+                                <div className="book-info">
+                                    <h2>{book.title}</h2>
+                                    <Link to={`/Books/${book.id}`}><button>View</button></Link>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </main>
+            <footer>
+                <p>Copyright &copy;</p>
+            </footer>
         </div>
     )
 }
