3a0fa4a12544a3a9a632eccb5725fba92b3c7368
Gabriel-Anareta <g.anareta231@mybvc.ca>
2024-12-06 15:40:33 -0700

    Styled top of home page

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -23,18 +23,38 @@ export const Home = () => {
             <header>
                 <div className={styles.Nav}>
                     <NavBar />
-                    <div className={styles.Title}>
+                </div>
+                <div className={styles.Title}>
+                    <div className={styles.TitleDarkener}>
                         <h1>Welcome {username}!</h1>
                     </div>
                 </div>
             </header>
             <main>
+                <section className={styles.BreakSection}>
+                    <div className={styles.QuoteContainer}>
+                        <div className={styles.TextBlock}>
+                            <p>
+                                “I am so impressed again with the life-giving
+                                <span className={styles.HighlightDark}> power of literature </span>
+                                . . .”
+                            </p>
+                            <p>
+                                “. . . If I were a young person today, 
+                                <span className={styles.HighlightDark}> trying to gain a sense of myself in the world</span>
+                                , would do that again by reading, just as I did when I was young.”
+                            </p>
+                            <p>~ Maya Angelou</p>
+                        </div>
+                        <button onClick={() => navigate('/Books')}>Find a book for you →</button>
+                    </div>
+                </section>
                 <section>
-                    {books.map(book => {
+                    {/* {books.map(book => {
                         return(
                             <BookDisplay key={book.id} data={book} />
                         )
-                    })}
+                    })} */}
                 </section>
             </main>
         </div>
